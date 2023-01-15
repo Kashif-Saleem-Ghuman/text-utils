798cc84958674976bfaa89bc9e9ff58bab2c2be7
unknown <kashif_saleem_23@hotmail.com>
2023-01-15 12:35:03 +0500

    add uppercase lowercase feature

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './Components/Navbar';
+import TextForm from './Components/TextForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar title="TextUtils" aboutText="About Text" />
+      <TextForm heading="Enter Text To Analyze below" />
     </div>
   );
 }
